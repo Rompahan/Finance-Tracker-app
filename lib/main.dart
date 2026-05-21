d0625c45b8b412d0cc7e69209049a4a1cab7c723
Ryuma <ryumabissembay@gmail.com>
2026-05-21 06:13:52 +0500

    chore: initialize Flutter project with dependencies

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Finance Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(child: Text('Finance Tracker')),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
